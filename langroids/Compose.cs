a079a54f8e3c245d6cc1ef1d3fe32f71279f62b2
= <jonathanschmold@gmail.com>
2017-10-20 13:12:29 -0600

    Expanded Compose

--- langroids/Compose.cs
+++ langroids/Compose.cs
@@ -30,8 +30,24 @@ public static partial class LangRoids {
     /// <typeparam name="T">The type to use</typeparam>
     /// <param name="arg">The item to operate on</param>
     /// <param name="pipeline">The array of functions to iterate over</param>
-    public static void Compose<T>(T arg, params Action<T>[] pipeline) => Repeat(pipeline.Length, i => pipeline[i](arg));
-
+    public static void Compose<T>(T arg, params Action<T>[] pipeline) 
+        => Repeat(pipeline.Length, i => pipeline[i](arg));
+	public static void Compose<T1, T2>(T1 arg1, T2 arg2, params Action<T1, T2>[] pipeline) 
+        => Repeat(pipeline.Length, i => pipeline[i](arg1, arg2));
+	public static void Compose<T1, T2, T3>(T1 arg1, T2 arg2, T3 arg3, params Action<T1, T2, T3>[] pipeline) 
+        => Repeat(pipeline.Length, i => pipeline[i](arg1, arg2, arg3));
+	public static void Compose<T1, T2, T3, T4>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, params Action<T1, T2, T3, T4>[] pipeline) 
+        => Repeat(pipeline.Length, i => pipeline[i](arg1, arg2, arg3, arg4));
+	public static void Compose<T1, T2, T3, T4, T5>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, params Action<T1, T2, T3, T4, T5>[] pipeline) 
+        => Repeat(pipeline.Length, i => pipeline[i](arg1, arg2, arg3, arg4, arg5));
+	public static void Compose<T1, T2, T3, T4, T5, T6>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, params Action<T1, T2, T3, T4, T5, T6>[] pipeline) 
+        => Repeat(pipeline.Length, i => pipeline[i](arg1, arg2, arg3, arg4, arg5, arg6));
+	public static void Compose<T1, T2, T3, T4, T5, T6, T7>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, params Action<T1, T2, T3, T4, T5, T6, T7>[] pipeline) 
+        => Repeat(pipeline.Length, i => pipeline[i](arg1, arg2, arg3, arg4, arg5, arg6, arg7));
+	public static void Compose<T1, T2, T3, T4, T5, T6, T7, T8>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, params Action<T1, T2, T3, T4, T5, T6, T7, T8>[] pipeline) 
+        => Repeat(pipeline.Length, i => pipeline[i](arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8));
+	public static void Compose<T1, T2, T3, T4, T5, T6, T7, T8, T9>(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, params Action<T1, T2, T3, T4, T5, T6, T7, T8, T9>[] pipeline) 
+        => Repeat(pipeline.Length, i => pipeline[i](arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9));
     /// <summary>
     /// Perform a set of operations if test is true on an object of T
     /// </summary>
@@ -39,5 +55,22 @@ public static partial class LangRoids {
     /// <param name="test">Whether or not the pipeline should be executed</param>
     /// <param name="arg">The item to operate on</param>
     /// <param name="pipeline">The array of functions to iterate over</param>
-    public static void Compose<T>(bool test, T arg, params Action<T>[] pipeline) => Perform(test, () => Compose(arg, pipeline));
+    public static void Compose<T>(bool test, T arg, params Action<T>[] pipeline) => 
+    	Perform(test, () => Compose(arg, pipeline));
+    public static void Compose<T1, T2>(bool test, T1 arg1, T2 arg2, params Action<T1, T2>[] pipeline) => 
+    	Perform(test, () => Compose(arg1, arg2, pipeline));
+    public static void Compose<T1, T2, T3>(bool test, T1 arg1, T2 arg2, T3 arg3, params Action<T1, T2, T3>[] pipeline) => 
+    	Perform(test, () => Compose(arg1, arg2, arg3, pipeline));
+    public static void Compose<T1, T2, T3, T4>(bool test, T1 arg1, T2 arg2, T3 arg3, T4 arg4, params Action<T1, T2, T3, T4>[] pipeline) => 
+    	Perform(test, () => Compose(arg1, arg2, arg3, arg4, pipeline));
+    public static void Compose<T1, T2, T3, T4, T5>(bool test, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, params Action<T1, T2, T3, T4, T5>[] pipeline) => 
+    	Perform(test, () => Compose(arg1, arg2, arg3, arg4, arg5, pipeline));
+    public static void Compose<T1, T2, T3, T4, T5, T6>(bool test, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6,  params Action<T1, T2, T3, T4, T5, T6>[] pipeline) => 
+    	Perform(test, () => Compose(arg1, arg2, arg3, arg4, arg5, arg6, pipeline));
+    public static void Compose<T1, T2, T3, T4, T5, T6, T7>(bool test, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, params Action<T1, T2, T3, T4, T5, T6, T7>[] pipeline) => 
+    	Perform(test, () => Compose(arg1, arg2, arg3, arg4, arg5, arg6, arg7, pipeline));
+    public static void Compose<T1, T2, T3, T4, T5, T6, T7, T8>(bool test, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, params Action<T1, T2, T3, T4, T5, T6, T7, T8>[] pipeline) => 
+    	Perform(test, () => Compose(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, pipeline));
+    public static void Compose<T1, T2, T3, T4, T5, T6, T7, T8, T9>(bool test, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, params Action<T1, T2, T3, T4, T5, T6, T7, T8, T9>[] pipeline) => 
+    	Perform(test, () => Compose(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, pipeline));
 }
