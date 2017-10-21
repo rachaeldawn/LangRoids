dca71e46b3a9c7d8eff79aebd6b9ac513c5d5f1e
= <jonathanschmold@gmail.com>
2017-10-21 12:19:29 -0600

    Added Filter function

---
+++ langroids/Collections.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public static partial class LangRoids {
+    /// <summary>
+    /// Filter everything failing the predicate, and return it as a new IEnumerable<typeparamref name="T"/>
+    /// </summary>
+    /// <typeparam name="T">The type to use</typeparam>
+    /// <param name="col">The collection to filter</param>
+    /// <param name="pred">The predicate to use in the filter process</param>
+    /// <returns></returns>
+    public static IEnumerable<T> Filter<T>(this IEnumerable<T> col, Predicate<T> pred) {
+        foreach (var obj in col) {
+            if (pred(obj)) {
+                yield return obj;
+            }
+        }
+    }
+}
---
+++ langroids_tests/Collections_Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static LangRoids;
+
+namespace langroids_tests
+{
+    [TestClass]
+    [TestCategory("Collections")]
+    public class Collections_Tests
+    {
+        [TestMethod]
+        public void Filter_FiltersAll() {
+            List<int> nums = new List<int>( );
+            Repeat(100, nums.Add);
+            ForEach(nums.Filter(i => i >= 50), num => Assert.IsTrue(num >= 50, $"Did not strip {num}"));
+        }
+    }
+}
